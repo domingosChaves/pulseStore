ad4cb39ab7a7f19350b71f52d242eb04fa8a36ac
Domingos Chaves <dudu.dekst18@gmail.com>
2025-03-24 15:15:11 -0300

    Rotina para criar usuario

--- src/main/java/com/meuprojetocheckout/pulseStore/controllers/UsuarioController.java
+++ src/main/java/com/meuprojetocheckout/pulseStore/controllers/UsuarioController.java
@@ -1,22 +1,35 @@
 package com.meuprojetocheckout.pulseStore.controllers;
 
+import com.meuprojetocheckout.pulseStore.models.LoginRequest;
 import com.meuprojetocheckout.pulseStore.models.Usuario;
 import com.meuprojetocheckout.pulseStore.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
+
     @Autowired
     private UsuarioService usuarioService;
 
-    @PostMapping
-    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
-        return ResponseEntity.ok(usuarioService.cadastrarUsuario(usuario));
+
+    @PostMapping("/registrar")
+    public ResponseEntity<Usuario> registrarUsuario(@RequestBody Usuario usuario) {
+        Usuario novoUsuario = usuarioService.cadastrarUsuario(usuario);
+        return ResponseEntity.ok(novoUsuario);
     }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuario) {
+        Usuario usuarioAtualizado = usuarioService.atualizarUsuario(id, usuario);
+        return ResponseEntity.ok(usuarioAtualizado);
+    }
+
 }
--- src/main/java/com/meuprojetocheckout/pulseStore/services/UsuarioService.java
+++ src/main/java/com/meuprojetocheckout/pulseStore/services/UsuarioService.java
@@ -3,6 +3,7 @@ package com.meuprojetocheckout.pulseStore.services;
 import com.meuprojetocheckout.pulseStore.models.Usuario;
 import com.meuprojetocheckout.pulseStore.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,10 +14,26 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    public Usuario cadastrarUsuario(Usuario usuario){
+//    @Autowired
+//    private PasswordEncoder passwordEncoder; // Para hash da senha
+
+    public Usuario cadastrarUsuario(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
-    public Optional<Usuario> buscarUsuarioPorEmail(String email){
+
+    public Optional<Usuario> consultarPorEmail(String email) {
         return Optional.ofNullable(usuarioRepository.findByEmail(email));
+    }
+
+    // Método para alterar dados ou recuperar senha
+    public Usuario atualizarUsuario(Long id, Usuario usuarioAtualizado) {
+        Optional<Usuario> usuarioOpt = usuarioRepository.findById(id);
+        if (usuarioOpt.isPresent()) {
+            Usuario usuario = usuarioOpt.get();
+            usuario.setNome(usuarioAtualizado.getNome());
+            // Atualize outros campos conforme necessário
+            return usuarioRepository.save(usuario);
+        }
+        throw new RuntimeException("Usuário não encontrado");
     }
 }
