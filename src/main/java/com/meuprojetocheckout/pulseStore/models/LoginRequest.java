945e4dee8607066dcec4a5a1dc85c0405555e0be
Domingos Chaves <dudu.dekst18@gmail.com>
2025-03-24 15:18:46 -0300

    Rotina de criptografia de senhas e autenticação de requisiçoes, comentado para dar seguimento no desenvolvimento, ao concluir as rotinas que requer auth habilito novamente

---
+++ src/main/java/com/meuprojetocheckout/pulseStore/models/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.meuprojetocheckout.pulseStore.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @NotBlank(message = "Email não pode estar vazio.")
+    @Email(message = "Formato de email inválido.")
+    private String email;
+
+    @NotBlank(message = "Senha não pode estar vazia.")
+    private String senha;
+}
