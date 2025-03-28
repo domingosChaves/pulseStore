ed77f28e17f8c61da0606b0d76b0408ee2849b84
Domingos Chaves <dudu.dekst18@gmail.com>
2025-03-27 22:56:53 -0300

    Removendo imports nao usados

--- src/main/java/com/meuprojetocheckout/pulseStore/repository/ClienteRepository.java
+++ src/main/java/com/meuprojetocheckout/pulseStore/repository/ClienteRepository.java
@@ -4,6 +4,7 @@ import com.meuprojetocheckout.pulseStore.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ClienteRepository  extends JpaRepository<Cliente,Long> {
-}
+public interface ClienteRepository  extends JpaRepository<Cliente,Long> { }
